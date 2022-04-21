a973aac3dd2b2e9a2061ff5331acf6b6a6f3d349
suganglive <88217758+suganglive@users.noreply.github.com>
2022-04-21 13:47:21 +0900

    ,s

--- dailycompare/pyupbase.py
+++ dailycompare/pyupbase.py
@@ -3,15 +3,15 @@ import numpy as np
 import pandas as pd
 import time
 
-def get_daily_ohlcv_from_base(ticker="KRW-BTC", base='10h'):
+def get_daily_ohlcv_from_base(ticker="KRW-BTC", base='11h'):
     """
     :param ticker:
     :param base:
     :return:
     """
     try:
-        # df = pyupbit.get_ohlcv(ticker, interval="minute60", count=300)
-        df = pyupbit.get_ohlcv(ticker, interval="minute60", count=8760)
+        df = pyupbit.get_ohlcv(ticker, interval="minute60", count=300)
+        # df = pyupbit.get_ohlcv(ticker, interval="minute60", count=8760)
         df = df.resample('24H', offset=base).agg(
             {'open': 'first', 'high': 'max', 'low': 'min', 'close': 'last', 'volume': 'sum'})
         # time.sleep(0.5)
